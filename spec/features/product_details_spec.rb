eb3479087ca259145c49472d5695c7421e6b1719
Overseer009 <sacha.sunnasy@gmail.com>
2021-06-29 16:39:33 -0400

    complete the product details tests

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.feature "ProductDetails", type: :feature, js: true do 
+  
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    1.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "see all product details" do
+    visit root_path
+
+    find("header a").click
+    sleep 5
+    save_screenshot
+    expect(page).to have_content 'Apparel'
+  end
+end
